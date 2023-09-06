f96ce9704996603fed0711f415e379b85236c39c
spacesparky <124715925+spacesparky@users.noreply.github.com>
2023-09-06 15:17:59 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static float convert(int x) {
         return x * 3.785f;
     }
+/*    kakoy nibud comment*/
     public static void main(String[] args) {
         float result = convert(5);
         System.out.println(result);
