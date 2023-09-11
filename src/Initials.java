a7be40f488bb38643cc2b298120d0d6d998a7ea3
spacesparky <124715925+spacesparky@users.noreply.github.com>
2023-09-11 14:27:23 +0300

    laba1 3

--- src/Initials.java
+++ src/Initials.java
@@ -1,19 +1,20 @@
 import static java.lang.Character.isUpperCase;
 
 public class Initials {
+
     public static void main(String[] args){
         System.out.println(getInitials("Ryan Gosling"));
         System.out.println(getInitials("Barack Obama"));
     }
     public static String getInitials(String str){
-        String ans = "";
-        ans = ans + str.charAt(0);
+        StringBuilder ans = new StringBuilder();
+        ans.append(str.charAt(0));
         for (int i=1; i<str.length();i++) {
             char currentChar = str.charAt(i);
             if (isUpperCase(currentChar)){
-                ans = ans + str.charAt(i);
+                ans.append(str.charAt(i));
             }
         }
-        return ans;
+        return ans.toString();
     }
 }
