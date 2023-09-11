4dd9e6ccd433d93603e8f01d2a5d9223cbfa7b75
spacesparky <124715925+spacesparky@users.noreply.github.com>
2023-09-11 14:26:15 +0300

    laba1 3

--- src/DiffEvo.java
+++ src/DiffEvo.java
@@ -1,16 +1,17 @@
 
 public class DiffEvo {
     public static void main(String[] args){
-        System.out.println(differenceEvenOdd({44, 32, 86, 19}));
-        System.out.println(differenceEvenOdd(({22, 50, 16, 63, 31, 55}));
+        System.out.println(differenceEvenOdd(new int[]{44, 32, 86, 19}));
+        System.out.println(differenceEvenOdd((new int[]{22, 50, 16, 63, 31, 55})));
     }
     public static int differenceEvenOdd(int[] array){
         int ans = 0;
-        for (int i=1; i<array.length;i++) {
-            if (array[i] % 2 == 0){
-                ans = ans + array[i];
+        for (int j : array) {
+            if (j % 2 == 0) {
+                ans = ans + j;
+            } else {
+                ans = ans - j;
             }
-            else {ans = ans - array[i];}
         }
         return ans;
     }
