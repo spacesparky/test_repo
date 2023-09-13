216e1296ea82c99ebe4b3e705cb32c14bd44523b
spacesparky <124715925+spacesparky@users.noreply.github.com>
2023-09-13 18:52:33 +0300

    laba2.6 task10fix

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,22 +1,23 @@
+import java.util.Arrays;
 public class Anagrams {
-        public static void main(String[] args) {
-            System.out.println(isAnagram("listen", "silent"));
-            System.out.println(isAnagram("eleven plus two", "twelve plus one"));
-            System.out.println(isAnagram("hello", "world"));
-        }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent"));
+        System.out.println(isAnagram("eleven plus two", "twelve plus one"));
+        System.out.println(isAnagram("hello", "world"));
+    }
 
-        public static boolean isAnagram(String str1, String str2) {
-            if (str1.length() != str2.length()) {
-                return false;
-            }
-
-            char[] charArray1 = str1.toCharArray();
-            char[] charArray2 = str2.toCharArray();
-            for (int i = 0; i<str1.length(); i++){
-                if (charArray1[i] != charArray2[charArray2.length - i-1]){
-                    return false;
-                }
-            }
-            return true;
+    public static boolean isAnagram(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+        if (str1.length() != str2.length()) {
+            return false;
         }
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
 }
